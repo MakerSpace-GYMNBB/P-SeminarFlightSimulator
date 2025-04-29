96aead66be87b5196547ce6238134b948218694c
Looter <example@example.com>
2025-04-29 16:14:51 +0200

    Arduino Read serial and return file added for test

---
+++ sketch_apr29a/sketch_apr29a.ino
@@ -0,0 +1,18 @@
+String incomingByte;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+        incomingByte = Serial.readStringUntil("\n");
+
+        Serial.print("I get ");
+        Serial.println(incomingByte);
+  }
+  
+
+}
