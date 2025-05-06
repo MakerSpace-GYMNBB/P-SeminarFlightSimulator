477f08a62fdddd580a2387151acf679e22675acb
Looter <example@example.com>
2025-05-06 17:29:26 +0200

    Serial Stuff int support

--- sketch_apr29a/sketch_apr29a.ino
+++ sketch_apr29a/sketch_apr29a.ino
@@ -1,4 +1,8 @@
+#include <stdlib.h>
+
 String incomingString;
+int values[2];
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,9 +14,33 @@ void loop() {
   if (Serial.available() > 0) {
         incomingString = Serial.readStringUntil("\n");
 
-        Serial.print("I get ");
-        Serial.println(incomingString);
+        //Serial.print("I get ");
+        //Serial.println(incomingString);
+        
+        int count = splitString(incomingString, ';', values, 2);
+        for (int i = 0; i < count; i++){
+          Serial.println(values[i]);
+        }
   }
+}
+
+int splitString(String input, char delimiter, int output[], int maxParts) {
+  int count = 0;
+  int startIndex = 0;
+  int delimIndex = input.indexOf(delimiter);
   
 
+  while (delimIndex >= 0 && count < maxParts - 1) {
+
+
+    
+    output[count++] = input.substring(startIndex).toInt();
+    startIndex = delimIndex + 1;
+    delimIndex = input.indexOf(delimiter, startIndex);
+  }
+
+  // Add the last part
+  output[count++] = input.substring(startIndex).toInt();
+  return count;
 }
+
